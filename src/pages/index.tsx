124e0f1ef4415a0930decdc8bdbf68093d7ba2c6
soramon0 <23435312+soramon0@users.noreply.github.com>
2022-10-17 11:50:13 +0100

    feat: display ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 
-import { Container, Typography } from "@mui/material";
+import { Container, Typography } from '@mui/material';
 
-import WalletTabs from "../components/WalletTabs";
+import WalletTabs from '../components/WalletTabs';
 
 const Home: NextPage = () => {
-  return null;
-
   return (
-    <Container maxWidth="xl">
-      <Typography py={4} variant="h4" component="h1">
+    <Container maxWidth='xl'>
+      <Typography py={4} variant='h4' component='h1'>
         XLD Wallet Connector SDK (Software Developed by Karim)
       </Typography>
       <WalletTabs />
